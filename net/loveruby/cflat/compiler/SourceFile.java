61cae56d2e80f018804f1d05259cd2286fc14000
yusei-wy <yusei.kasa@gmail.com>
2022-12-10 14:54:46 +0000

    add: path() method

--- net/loveruby/cflat/compiler/SourceFile.java
+++ net/loveruby/cflat/compiler/SourceFile.java
@@ -1,5 +1,15 @@
 package net.loveruby.cflat.compiler;
 
 public class SourceFile {
+    private final String originalName;
+    private String currentName;
 
+    SourceFile(String name) {
+        this.originalName = name;
+        this.currentName = name;
+    }
+
+    String path() {
+        return this.currentName;
+    }
 }
